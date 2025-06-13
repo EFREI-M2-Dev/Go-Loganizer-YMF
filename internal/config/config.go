a5f3d4228c60a4e347d0c0131875ffa4a971b8b5
Florent Paris <florentparis28@gmail.com>
2025-06-13 17:03:13 +0200

    feat(config): add loadTarget func

---
+++ internal/config/config.go
@@ -0,0 +1,25 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type InputTarget struct {
+	Id 	  string `json:"id"`
+	Path  string `json:"path"`
+	Type  string `json:"type"`
+}
+
+func LoadTargetsFromFile(filePath string) ([]InputTarget, error) {
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read file %s: %w", filePath, err)
+	}
+	var targets []InputTarget
+	if err := json.Unmarshal(data, &targets); err != nil {
+		return nil, fmt.Errorf("failed to unmarshall from file %s: %w", filePath, err)
+	}
+	return targets, nil
+}
